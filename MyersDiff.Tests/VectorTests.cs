1810692b57c7cc51d31642fb9f6956331fc6c190
glokhov <105565846+glokhov@users.noreply.github.com>
2026-03-25 21:59:19 +0100

    initialize project

---
+++ MyersDiff.Tests/VectorTests.cs
@@ -0,0 +1,19 @@
+﻿namespace MyersDiff.Tests;
+
+public class VectorTests
+{
+    [Fact]
+    public void VectorTest()
+    {
+        // ReSharper disable once UseObjectOrCollectionInitializer
+        var vector = new Vector<int>(1);
+
+        vector[-1] = 1;
+        vector[0] = 2;
+        vector[1] = 3;
+
+        Assert.Equal(1, vector[-1]);
+        Assert.Equal(2, vector[0]);
+        Assert.Equal(3, vector[1]);
+    }
+}
---
+++ MyersDiff/Vector.cs
@@ -0,0 +1,12 @@
+﻿namespace MyersDiff;
+
+internal class Vector<T>(int length)
+{
+    private readonly T[] _vector = new T[length * 2 + 1];
+
+    public T this[int i]
+    {
+        get => _vector[i + length];
+        set => _vector[i + length] = value;
+    }
+}
